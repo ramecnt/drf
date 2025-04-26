5140f7f6d5452a50e6f4d5ada32a8bfb0af96228
nikolay <n.ilin@it-park.tech>
2025-04-26 12:07:26 +0300

    requirements.txt updated

--- drf/settings.py
+++ drf/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
