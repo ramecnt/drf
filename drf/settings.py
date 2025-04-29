f8df37af162e2c6e422d1f04a52b7ac969287b0e
nikolay <n.ilin@it-park.tech>
2025-04-29 11:52:11 +0300

    fixed

--- drf/settings.py
+++ drf/settings.py
@@ -90,6 +90,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
         'PORT': os.getenv('DATABASE_PORT'),
     }
 }
